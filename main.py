7d2cf81f6d2e896cb61a9ce4fa89a7bd3438200e
adition2015 <voltaprime2000@outlook.com>
2025-09-18 19:14:20 +0100

    Functional grid filling

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# Goals for this version
+# Make the window resizable, with the grid being 80% of the screen when resized.
+
 # Imports
 import pygame, sys
 
 # Variables
 w, h = 800, 800
-cw, ch = 20, 20
+x, y = 20, 20 # size in cells
+grid_size = 0.8 #as proportion of screen (0 <= x <= 1)
+flags = pygame.RESIZABLE 
 pygame.init()
-screen = pygame.display.set_mode((w, h))
+screen = pygame.display.set_mode((w, h), flags)
 clock = pygame.time.Clock()
 
 # Mainloop
@@ -13,12 +18,18 @@ def main():
     while True:
         # Poll for events
         for event in pygame.event.get():
+            # Handles exit
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit()   
+                sys.exit()
+            # Handles resizing
+            if event.type == pygame.VIDEORESIZE:
+                neww, newh = event.w, event.h
+                resize_grid(neww, newh, grid_size)
+                pygame.display.flip()
         # Render visualiser   
         screen.fill("black")
-        fill_grid(cw, ch)
+        fill_grid(x, y, grid_size)
 
 
         # Flips the display to put work on screen:
@@ -27,14 +38,39 @@ def main():
         clock.tick(60) # limits FPS to 60
 
 
-# Fills the screen with a x, y grid
-def fill_grid(x, y):
+# Fills 80% of the screen with a x, y grid at the center
+# grid stays square
+"""def fill_grid(x, y):
     global w, h
-    cw, ch = w/x, h/x
+    size = 0.8 # relative to screen
+    cw, ch = (size*w)//x, (size*h)//x
+    offset = (1-size)/2
     for i in range(x+1):        
-        pygame.draw.line(screen, "cyan", ((i)*cw, 0), ((i)*cw, h))
+        pygame.draw.line(screen, "cyan", (i*cw+(offset*w), offset*h), (i*cw+(offset*w), (1-offset)*h))
     for j in range(y+1):
-        pygame.draw.line(screen, "cyan", (0, (j)*ch), (w, (j)*ch))
+        pygame.draw.line(screen, "cyan", (offset*w, j*ch+(offset*h)), ((1-offset)*w, j*ch+(offset*h)))
+"""
+
+def fill_grid(x, y, grid_size):
+    global w, h
+    min_dim = min(w, h) # returns integer for 
+    # base offsets from the smaller dimension
+    cw, ch = int(grid_size*min_dim//x), int(grid_size*min_dim//y)
+    size = grid_size*min_dim
+    offsetw, offseth = (w-size)/2, (h-size)/2
+    for i in range(x+1):
+        pygame.draw.line(screen, "cyan", (offsetw + (i*cw), offseth), (offsetw + (i*cw), offseth+size))
+    for j in range(y+1):
+        pygame.draw.line(screen, "cyan", (offsetw, offseth + (j*ch)), (offsetw + size, offseth + (j*ch)))
+    
+
+def resize_grid(neww, newh, grid_size):
+    global w, h
+    w, h = neww, newh
+    newx, newy = int(grid_size*neww//x), int(grid_size*newh//y)
+    screen.fill("black")
+    fill_grid(newx, newy, grid_size)
+    
 
 # Tests
 if __name__ == "__main__":
